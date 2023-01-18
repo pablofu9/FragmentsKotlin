627edcf67d510130b7416530639c22ef73852238
Pablofu9 <pablofuertesruiz@gmail.com>
2023-01-18 12:21:44 +0100

    Fragments

--- app/src/main/java/com/example/practica17/activities/MainActivity.kt
+++ app/src/main/java/com/example/practica17/activities/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.practica17.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.practica17.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        var fragment: Fragment? = null
+        fragment = ImageFragment()
+        val args = Bundle()
+        args.putInt(resources.getString(R.string.image), item.imageId)
+        fragment.arguments = args
+// Añade el fragmento
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.add(R.id.layout, fragment)
+        transaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/practica17/fragments/FragmentDatos.kt
@@ -0,0 +1,40 @@
+package com.example.practica17.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.example.practica17.R
+
+
+
+class FragmentDatos : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Carga el layout del fragmento
+        val view = inflater.inflate(R.layout.fragment_fotos, container, false)
+
+        // Si ha recibido argumentos
+        arguments?.let {
+            // Obtiene el id de la imagen
+            val imageId = requireArguments().getInt(resources.getString(R.string.image))
+            // Obtiene la vista de imagen
+            val imageView = view.findViewById<ImageView>(R.id.imageView)
+            // Carga la imagen en la vista
+            imageView.setImageDrawable(context?.getDrawable(imageId))
+        }
+        return view
+
+    }
+}
