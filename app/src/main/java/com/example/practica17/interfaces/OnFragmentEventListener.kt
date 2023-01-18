c4e0bef1f72afcdba3a9ee2ea6e7d5de3d8605eb
Pablofu9 <pablofuertesruiz@gmail.com>
2023-01-18 11:44:40 +0100

    Fragments

---
+++ app/src/main/java/com/example/practica17/interfaces/OnFragmentEventListener.kt
@@ -0,0 +1,7 @@
+package com.example.practica17.interfaces
+
+import com.example.practica17.model.Alumno
+
+interface OnFragmentEventListener {
+    fun onFragmentEvent(alumno: Alumno)
+}
---
+++ app/src/main/java/com/example/practica17/model/Alumno.kt
@@ -0,0 +1,47 @@
+package com.example.practica17.model
+
+class Alumno: java.io.Serializable {
+    var name:String=""
+    var apellido:String=""
+    var DNI:String=""
+    var isImage = false
+    var text: String? = null
+    var imageId = 0
+
+    constructor()
+    constructor(
+        name: String,
+        apellido: String,
+        DNI: String,
+        isImage: Boolean,
+        text: String?,
+        imageId: Int
+    ) {
+        this.name = name
+        this.apellido = apellido
+        this.DNI = DNI
+        this.isImage = isImage
+        this.text = text
+        this.imageId = imageId
+    }
+
+    constructor(name: String, apellido: String, DNI: String, text: String?) {
+        this.name = name
+        this.apellido = apellido
+        this.DNI = DNI
+        this.text = text
+    }
+
+    constructor(name: String, apellido: String, DNI: String, imageId: Int) {
+        this.name = name
+        this.apellido = apellido
+        this.DNI = DNI
+        this.imageId = imageId
+    }
+
+    override fun toString(): String {
+        return "Alumno(name='$name', apellido='$apellido', DNI='$DNI', isImage=$isImage, text=$text, imageId=$imageId)"
+    }
+
+
+}
